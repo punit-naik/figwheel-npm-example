9ac2e42549c78599d017661fd14ab4eb1205cfd0
Punit Naik <naik.punit44@gmail.com>
2022-01-12 00:32:12 +0530

    printed left padded string in console when website is loaded to show that the npm dependency is working

--- src/org/clojars/punit_naik/figwheel_npm_example/core.cljs
+++ src/org/clojars/punit_naik/figwheel_npm_example/core.cljs
@@ -3,11 +3,12 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/org/clojars/punit_naik/figwheel-npm-example/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;; define your app data so that it doesn't get over-written on reload
 
 (def app-state (atom {:text (str "\"" (lp "123" 6) "\"")}))
+
+(println "This text is printed from src/org/clojars/punit_naik/figwheel-npm-example/core.cljs. Go ahead and edit it and see reloading in action."
+         (:text @app-state))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
